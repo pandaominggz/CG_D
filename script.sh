c50f075484c73f43a935688aacfe9da3903f981b
pandaominggz <pandaominggz@qq.com>
2020-02-23 03:37:14 +0000

    Initial commit

---
+++ script.sh
@@ -0,0 +1,7 @@
+# /home/ubuntu/miniconda3/bin/python3.7
+
+# sudo scp -r ubuntu@172.17.0.6:/home/ubuntu/CG_D /root/
+
+# ssh root@49.234.217.136
+
+#  scp -r root@49.234.217.136:/root/CG_D /home/daoming/Desktop/result/
